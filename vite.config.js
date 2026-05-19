ef05d09a075a0af7b993295a3921ca3f5c1dfc3c
WolcenOn <fhaldon@gmail.com>
2026-05-19 11:13:32 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/it-tools-dashboard/",
+  base: "/It-tools-dashboard/",
   plugins: [react(), tailwindcss()],
 });
